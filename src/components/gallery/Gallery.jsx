aba083b553debda23d36c24bc5ede870a65511f4
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-01-26 14:41:59 -0300

    acrescentado botões de navegação na galeria

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Gallery.css';
 
 const Gallery = ({ images }) => {
@@ -13,11 +13,46 @@ const Gallery = ({ images }) => {
         setSelectedImage(null);
     };
 
+    const handlePrev = () => {
+        const currentIndex = images.indexOf(selectedImage);
+        const prevIndex = currentIndex - 1;
+        if (prevIndex >= 0) {
+            setSelectedImage(images[prevIndex]);
+        }
+    };
+    
+    const handleNext = () => {
+        const currentIndex = images.indexOf(selectedImage);
+        const nextIndex = currentIndex + 1;
+        if (nextIndex < images.length) {
+            setSelectedImage(images[nextIndex]);
+        }
+    };
+    
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (selectedImage) {
+                if (e.keyCode === 37) {
+                    handlePrev();
+                }
+                if (e.keyCode === 39) {
+                    handleNext();
+                }
+            }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [selectedImage, handlePrev, handleNext]);
+
     return (
         <div className="gallery-container">
             {selectedImage ? (
                 <div className="selected-image-container">
                     <img src={selectedImage} alt="Selected" className="selected-image" />
+                    <button className="prev-button" onClick={handlePrev}>&lt;</button>
+                    <button className="next-button" onClick={handleNext}>&gt;</button>
                     <button className="close-button" onClick={handleClose}>
             X
                     </button>
