c9680430e1e3a32e6543e186a3c49f83781d0daf
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-01-26 14:05:28 -0300

    alteração na responsividado componente redessociais

--- src/components/nossosProdutos/NossosProdutos.jsx
+++ src/components/nossosProdutos/NossosProdutos.jsx
@@ -7,19 +7,19 @@ function NossosProdutos() {
             <h1 className="products__title">nossos produtos</h1>
             <ul className="products__list">
                 <li className="products__item">
-                    <a href="#" className="products__link">Todos produtos</a>
+                    <a href="/produtos" className="products__link">Todos produtos</a>
                 </li>
                 <li className="products__item">
-                    <a href="#" className="products__link">bolos</a>
+                    <a href="/produtos" className="products__link">bolos</a>
                 </li>
                 <li className="products__item">
-                    <a href="#" className="products__link">tortas</a>
+                    <a href="/produtos" className="products__link">tortas</a>
                 </li>
                 <li className="products__item">
-                    <a href="#" className="products__link">doces</a>
+                    <a href="/produtos" className="products__link">doces</a>
                 </li>
                 <li className="products__item">
-                    <a href="#" className="products__link">presentes</a>
+                    <a href="/produtos" className="products__link">presentes</a>
                 </li>
             </ul>
         </div>
