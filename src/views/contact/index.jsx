efedec2c531afcbb6ee25387daf9ddc598b0ec24
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-01-27 13:44:44 -0300

    utilizado atributo order em contatos, alterada margin do header

--- src/views/contact/index.jsx
+++ src/views/contact/index.jsx
@@ -13,6 +13,22 @@ function Contact() {
                 <h1 className="contato__title">Contato</h1>
 
                 <div className="contato__content">
+                    
+
+                    <div className="contato__maps">
+                        <h2>Google Maps:</h2>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.639542078802!2d-43.984014712605216!3d-19.939589007281366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa697ab7a0c86b1%3A0x7a57712a8f8a65e8!2sConfeitaria%20Glayde%20Ribeiro!5e0!3m2!1spt-BR!2sbr!4v1674829676319!5m2!1spt-BR!2sbr"
+                            width="400"
+                            height="300"
+                            frameBorder="0"
+                            style={{border:0}}
+                            allowFullScreen=""
+                            aria-hidden="false"
+                            tabIndex="0"
+                        />
+                    </div>
+
                     <div className="contact__right">
                         <div className="contato__endereco">
                             <h2>Endereço:</h2>
@@ -28,20 +44,6 @@ function Contact() {
                         </div>
                         <div><a target="_blank" href="https://wa.me/5531991525735?text=Ol%C3%A1%20Glayde,%20te%20encontrei%20atrav%C3%A9s%20do%20seu%20site!" rel="noreferrer">
                             <button className="botao__whatsapp">Chamar no WhatsApp</button></a></div>
-                    </div>
-
-                    <div className="contato__maps">
-                        <h2>Google Maps:</h2>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.639542078802!2d-43.984014712605216!3d-19.939589007281366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa697ab7a0c86b1%3A0x7a57712a8f8a65e8!2sConfeitaria%20Glayde%20Ribeiro!5e0!3m2!1spt-BR!2sbr!4v1674829676319!5m2!1spt-BR!2sbr"
-                            width="400"
-                            height="300"
-                            frameBorder="0"
-                            style={{border:0}}
-                            allowFullScreen=""
-                            aria-hidden="false"
-                            tabIndex="0"
-                        />
                     </div>
                 </div>
                 
